7a2fc38e6abf57e08f44339d0289beb547aa9796
Alessandro Morvillo <a.morvillo@amdev.it>
2023-08-31 15:14:51 +0200

    Added PassyCoreUtils

---
+++ PassyCoreUtils.xcframework/ios-arm64_x86_64-simulator/PassyCoreUtils.framework/Headers/PassyCoreUtils.h
@@ -0,0 +1,18 @@
+//
+//  PassyCoreUtils.h
+//  PassyCoreUtils
+//
+//  Created by Alessandro Morvillo on 15/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PassyCoreUtils.
+FOUNDATION_EXPORT double PassyCoreUtilsVersionNumber;
+
+//! Project version string for PassyCoreUtils.
+FOUNDATION_EXPORT const unsigned char PassyCoreUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PassyCoreUtils/PublicHeader.h>
+
+
